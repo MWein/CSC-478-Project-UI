d3d9d04ff55aca05fbc84e13d736c8d812e0b780
Minuteman92 <mweinberg21@gmail.com>
2018-03-25 19:30:29 -0500

    Finished layout for login panel

--- src/components/LoginPanel.jsx
+++ src/components/LoginPanel.jsx
@@ -1,26 +1,70 @@
-//import Button from 'material-ui/Button'
+import Button from 'material-ui/Button'
 //import Checkbox from 'material-ui/Checkbox'
 //import { FormControlLabel } from 'material-ui/Form'
 import Paper from 'material-ui/Paper'
 import PropTypes from 'prop-types'
 import React from 'react'
-//import TextField from 'material-ui/TextField'
-//import Typography from 'material-ui/Typography'
+import TextField from 'material-ui/TextField'
 //import { actions as appActions } from '../redux/actions/appActions'
 import { connect } from 'react-redux'
+import Grid from 'material-ui/Grid'
+
 
 const LoginPanel = () => {
-  const paperPadding = '10px'
+  const paperPadding = '30px'
   const paddingStyle = {
+    width: '300px',
     paddingTop: paperPadding,
     paddingBottom: paperPadding,
     paddingLeft: paperPadding,
     paddingRight: paperPadding,
   }
 
+  const textFieldStyle = {
+    width: '100%',
+  }
+
   return (
     <Paper style={paddingStyle}>
-      Login
+      <div style={{ fontSize: 20 }}>
+        Login
+      </div>
+
+      <Grid container>
+
+        <Grid item xs={12}>
+          <TextField
+            id='name'
+            label='Username'
+            margin='normal'
+            style={textFieldStyle}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            autoComplete='current-password'
+            id='password-input'
+            label='Password'
+            margin='normal'
+            style={textFieldStyle}
+            type='password'
+          />
+        </Grid>
+
+        <Grid item xs={8}>
+          <Button color='primary'>
+            Forgot Password
+          </Button>
+        </Grid>
+
+        <Grid item xs={4}>
+          <Button color='primary'>
+            Login
+          </Button>
+        </Grid>
+
+      </Grid>
     </Paper>
   )
 }
