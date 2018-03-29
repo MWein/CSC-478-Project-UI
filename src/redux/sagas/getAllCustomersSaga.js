d8d54bc626f251924542d1d7df7ce491ef59a88f
Mike <mweinberg21@gmail.com>
2018-03-28 20:46:52 -0500

    Added getAllCustomers saga

---
+++ src/redux/sagas/getAllCustomersSaga.js
@@ -0,0 +1,37 @@
+import {
+  call,
+  put as dispatch,
+  select,
+  takeLatest,
+} from 'redux-saga/effects'
+import { actions as customerLookupActions } from '../actions/customerLookupActions'
+import { getToken } from '../selectors'
+import { post } from './helpers/makeFetchCall'
+
+export function* getAllCustomersSaga() {
+  const url = 'http://csc478team301.uisad.uis.edu:8080/allCustomers'
+  const token = yield select(getToken)
+  const body = {
+    token,
+  }
+
+  const response = yield call(post, {
+    url,
+    body: JSON.stringify(body),
+  })
+
+  if (response.payload.error) {
+    console.log('Error ', response.payload.errorMsg)
+  } else {
+    yield dispatch(customerLookupActions.setCustomers(response.payload.rows))
+  }
+}
+
+export default function* () {
+  yield takeLatest(
+    [
+      customerLookupActions.getAllCustomers().type,
+    ],
+    getAllCustomersSaga
+  )
+}
