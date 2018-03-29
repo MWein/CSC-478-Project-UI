e45500a56b701ecb0eaf2cc59791edcb1e701462
Mike <mweinberg21@gmail.com>
2018-03-28 20:46:40 -0500

    Added getToken selector

--- src/redux/selectors/index.js
+++ src/redux/selectors/index.js
@@ -1,3 +1,5 @@
+export const getToken = state => state.app.token
+
 export const getUsername = state => state.login.username
 export const getPassword = state => state.login.password
 export const getSecAnswer = state => state.login.securityAnswer
