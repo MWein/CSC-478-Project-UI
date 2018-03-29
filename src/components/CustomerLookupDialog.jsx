b1589efdcff02555e920b7aa8a0eec5098497dd9
Mike <mweinberg21@gmail.com>
2018-03-29 16:09:09 -0500

    Finished migration... hopefully this works when I get home

--- src/components/CustomerLookupDialog.jsx
+++ src/components/CustomerLookupDialog.jsx
@@ -1,32 +1,20 @@
 import Dialog, {
   DialogActions,
-  DialogContent,
   DialogTitle,
 } from 'material-ui/Dialog'
 import Button from 'material-ui/Button'
-import Grid from 'material-ui/Grid'
 import PropTypes from 'prop-types'
 import React from 'react'
-import TextField from 'material-ui/TextField'
+import addEditCustomer from './CustomerLookupDialogContents/addEditCustomer'
 import { connect } from 'react-redux'
 import { actions as customerLookupActions } from '../redux/actions/customerLookupActions'
+import searchCustomer from './CustomerLookupDialogContents/searchCustomer'
 
 
 const CustomerLookupDialog = ({
   open,
-  fName,
-  lName,
-  phone,
-  email,
-  address,
   selectedCustomer,
-  customerList,
-  filteredCustomers,
-  notFound,
   mode,
-  setSelectedCustomer,
-  setNotFound,
-  setFilteredCustomers,
   setFirstName,
   setLastName,
   setPhoneNumber,
@@ -35,138 +23,6 @@ const CustomerLookupDialog = ({
   setMode,
   closeCustomerLookup,
 }) => {
-  const customerListTable = () => {
-    const customerTable = filteredCustomers.map(customer => (
-      <Button
-        color='primary'
-        key={customer.id}
-        onClick={() => setSelectedCustomer(customer)}
-        style={{ width: '100%' }}
-        variant={customer === selectedCustomer ? 'raised' : 'flat'}
-      >
-        {customer.f_name} {customer.l_name} - {customer.phone}
-      </Button>
-    ))
-
-    const addNewCustomerButton = () => (
-      <Button onClick={() => setMode('add')} variant='raised'>
-        Add New Customer
-      </Button>
-    )
-
-    return customerTable.length > 0 ? (
-      <div>
-        {customerTable}
-        <br /><br />
-        {addNewCustomerButton()}
-      </div>
-    ) :
-      (
-        <div>
-          {notFound ? (<div><div style={{ color: 'red' }}>Customer Not Found</div><br /></div>) : null}
-          {addNewCustomerButton()}
-        </div>
-      )
-  }
-
-
-  const searchCustomers = () => {
-    const newFilteredCustomers = phone === '' && lName === '' ? [] :
-      customerList.filter(customer => customer.phone.includes(phone) && customer.l_name.toLowerCase().includes(lName.toLowerCase()))
-
-    if (newFilteredCustomers.length === 0) {
-      setNotFound(true)
-    } else {
-      setNotFound(false)
-    }
-
-    setFilteredCustomers(newFilteredCustomers)
-  }
-
-
-  const searchCustomerDialog = () => (
-    <div>
-      <DialogContent>
-
-        <TextField
-          label='Phone Number'
-          onChange={event => setPhoneNumber(event.target.value)}
-          value={phone}
-        />
-
-        &nbsp;&nbsp;&nbsp;
-        OR
-        &nbsp;&nbsp;&nbsp;&nbsp;
-
-        <TextField
-          label='Last Name'
-          onChange={event => setLastName(event.target.value)}
-          value={lName}
-        />
-
-        &nbsp;&nbsp;
-
-        <Button onClick={searchCustomers} variant='raised'>
-          Search
-        </Button>
-
-        <br /><br />
-        {customerListTable()}
-
-      </DialogContent>
-    </div>
-  )
-
-  const addEditCustomerDialog = () => (
-    <div>
-      <DialogContent>
-        <Grid container>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              label='First Name'
-              onChange={event => setFirstName(event.target.value)}
-              value={fName}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              label='Last Name'
-              onChange={event => setLastName(event.target.value)}
-              value={lName}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              label='Phone Number'
-              onChange={event => setPhoneNumber(event.target.value)}
-              value={phone}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              label='Email'
-              onChange={event => setEmail(event.target.value)}
-              value={email}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              label='Address'
-              onChange={event => setAddress(event.target.value)}
-              value={address}
-            />
-          </Grid>
-        </Grid>
-      </DialogContent>
-    </div>
-  )
-
-
   const selectAction = () => {
     setMode('edit')
     setFirstName(selectedCustomer.f_name)
@@ -207,7 +63,7 @@ const CustomerLookupDialog = ({
   return (
     <Dialog aria-labelledby='form-dialog-title' open={open}>
       <DialogTitle id='form-dialog-title'>Customer Lookup</DialogTitle>
-      {mode === '' ? searchCustomerDialog() : addEditCustomerDialog()}
+      {mode === '' ? (<searchCustomer />) : (<addEditCustomer />)}
 
       <DialogActions style={{ marginRight: '20px', marginBottom: '20px' }}>
         <Button
@@ -231,42 +87,24 @@ const CustomerLookupDialog = ({
 
 
 CustomerLookupDialog.propTypes = {
-  address: PropTypes.string.isRequired,
   closeCustomerLookup: PropTypes.func.isRequired,
-  customerList: PropTypes.array.isRequired,
-  email: PropTypes.string.isRequired,
-  fName: PropTypes.string.isRequired,
-  filteredCustomers: PropTypes.array.isRequired,
-  lName: PropTypes.string.isRequired,
   mode: PropTypes.string.isRequired,
-  notFound: PropTypes.bool.isRequired,
   open: PropTypes.bool.isRequired,
-  phone: PropTypes.string.isRequired,
   selectedCustomer: PropTypes.object.isRequired,
   setAddress: PropTypes.func.isRequired,
   setEmail: PropTypes.func.isRequired,
-  setFilteredCustomers: PropTypes.func.isRequired,
   setFirstName: PropTypes.func.isRequired,
   setLastName: PropTypes.func.isRequired,
   setMode: PropTypes.func.isRequired,
-  setNotFound: PropTypes.func.isRequired,
   setPhoneNumber: PropTypes.func.isRequired,
-  setSelectedCustomer: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = state => ({
-  customerList: state.customerLookup.customers,
   open: state.customerLookup.open,
   selectedCustomer: state.customerLookup.selectedCustomer,
-  phone: state.customerLookup.phone,
-  fName: state.customerLookup.fName,
-  lName: state.customerLookup.lName,
-  email: state.customerLookup.email,
-  address: state.customerLookup.address,
-  filteredCustomers: state.customerLookup.filteredCustomers,
-  notFound: state.customerLookup.notFound,
   mode: state.customerLookup.mode,
 })
+
 
 const actions = {
   ...customerLookupActions,
