5e2e90858cd40b956b546f8273561ef529569676
Mike <mweinberg21@gmail.com>
2018-03-28 20:47:53 -0500

    Created a basic router in AppContainer

--- src/containers/AppContainer.jsx
+++ src/containers/AppContainer.jsx
@@ -1,14 +1,44 @@
 import CustomerLookupDialog from '../components/CustomerLookupDialog'
 import LoadingDialog from '../components/LoadingDialog'
-//import LoginContainer from './LoginContainer'
+import LoginContainer from './LoginContainer'
+import PropTypes from 'prop-types'
 import React from 'react'
+import { connect } from 'react-redux'
 //import TransactionContainer from './TransactionContainer'
 
-const AppContainer = () => (
-  <div style={{ display: 'flex', justifyContent: 'center' }}>
-    <LoadingDialog />
-    <CustomerLookupDialog />
-  </div>
-)
 
-export default AppContainer
+const AppContainer = ({
+  token,
+}) => {
+  const router = () => {
+    if (token === '') {
+      return (
+        <LoginContainer />
+      )
+    }
+
+    return (
+      <CustomerLookupDialog />
+    )
+  }
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <LoadingDialog />
+      {router()}
+    </div>
+  )
+}
+
+AppContainer.propTypes = {
+  token: PropTypes.string.isRequired,
+}
+
+const mapStateToProps = state => ({
+  token: state.app.token,
+})
+
+const actions = {
+}
+
+export default connect(mapStateToProps, actions)(AppContainer)
