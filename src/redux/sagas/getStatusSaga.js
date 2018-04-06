366cf87df4452620009113d15086183a9705aab6
Mike <mweinberg21@gmail.com>
2018-04-06 01:42:55 -0500

    Status only prints in development environment

--- src/redux/sagas/getStatusSaga.js
+++ src/redux/sagas/getStatusSaga.js
@@ -3,11 +3,14 @@ import {
 } from 'redux-saga/effects'
 import { get } from './helpers/makeFetchCall'
 import getServerURL from './helpers/getServerURL'
+import os from 'os'
 
 export function* getStatusSaga() {
   const url = `${getServerURL()}/status`
 
   const response = yield call(get, { url })
 
-  console.log(response)
+  if (os.hostname() === 'localhost') {
+    console.log(response)
+  }
 }
